50199fe3ce283b79a4ae0547fb63ee0a53cca8fe
tyronegit <tatatkins@gmail.com>
2020-08-22 20:15:42 -0400

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by ta 8/19/2020.
+ */
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by ta 8/19/2020.
+ */
+
+public class Vet extends Person{
+}
